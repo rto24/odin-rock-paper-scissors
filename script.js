dc7306636751d30dd2fe8586bbdfa999a481a7b9
Richard <richardto180@gmail.com>
2023-11-17 12:55:48 -0500

    Fixed playerSelection buttons to return the correct result message

--- script.js
+++ script.js
@@ -1,53 +1,34 @@
-function getComputerChoice() {
-  let computerSelection = '';
-  let randomNum = Math.floor(Math.random() * 3 + 1);
-  if (randomNum === 1) {
-    computerSelection === 'Rock';
-  } else if (randomNum === 2) {
-    computerSelection === 'Paper';
-  } else {
-    computerSelection === 'Scissors';
-  }
-  return computerSelection;
-};
-
-/*
-let playerInput = (prompt ("Choose Rock, Paper, or Scissors"));
-*/
-
-let computerSelection = '';
-
 document.querySelector('.rock-btn').addEventListener('click', () => {
-  playRound('Rock', computerSelection);
+  playRound('Rock');
 });
 
 document.querySelector('.paper-btn').addEventListener('click', () => {
-  playRound('Paper', computerSelection);
+  playRound('Paper');
 });
 
 document.querySelector('.scissors-btn').addEventListener('click', () => {
-  playRound('Scissors', computerSelection);
+  playRound('Scissors');
 });
 
-
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
   
+  const computerSelection = getComputerChoice();
   let result = '';
   
-  if (playerSelection === 'Rock') {
+  if (playerSelection == 'Rock') {
     if (computerSelection === 'Rock') {
       result = "It's a Tie!";
     } else if (computerSelection === 'Paper') {
       result = "You Lose! Paper beats Rock";
-    } else (computerSelection === 'Scissors' )
+    } else if (computerSelection === 'Scissors' )
       result = "You Win! Rock beats Scissors";
 
-    } else if (playerSelection === 'Paper') {
+    } else if (playerSelection == 'Paper') {
       if (computerSelection === 'Rock') {
         result = "You Win! Paper beats Rock";
       } else if (computerSelection === 'Paper') {
         result = "It's a Tie!";
-      } else (computerSelection === 'Scissors' )
+      } else if (computerSelection === 'Scissors' )
         result = "You Lose! Scissors beats Paper";
 
     } else { 
@@ -55,18 +36,33 @@ function playRound(playerSelection, computerSelection) {
         result = "You Lose! Rock beats Scissors";
       } else if (computerSelection === 'Paper') {
         result = "You Win! Scissors beats Paper";
-      } else (computerSelection === 'Scissors' )
+      } else if (computerSelection === 'Scissors' )
         result = "It's a Tie!";
     }
-    return result;
+    
+    document.querySelector('.result').textContent = result; 
+   
 };
-
-
 
 let score = {
   playerScore: 0,
   computerScore: 0
 };
+
+function getComputerChoice() {
+  let computerSelection = '';
+  const randomNum = Math.floor(Math.random() * 3 + 1);
+  if (randomNum === 1) {
+    computerSelection = 'Rock';
+  } else if (randomNum === 2) {
+    computerSelection = 'Paper';
+  } else {
+    computerSelection ='Scissors';
+  }
+  return computerSelection;
+};
+
+
 
 /*
 function game() {
